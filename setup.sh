85e127efe403e2559a5350d8b2b16e6116147e9f
Ubuntu <ubuntu@ip-172-31-27-74.ec2.internal>
2025-09-23 05:37:04 +0000

    17

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# setting up node
+sudo apt update
+sudo apt install curl git -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+npm install -g npm@latest
+npm install --global yarn
+
+
+#settings up playwright
+npx playwright install --with-deps
+npx playwright install webkit
+
+
+# setting up project
+yarn
